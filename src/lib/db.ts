e5c283a557f50d9477247208524e64d78abe740a
Issa Jean Marie <JeanMarieIssa@gmail.com>
2026-06-01 00:11:25 +0200

    Move to neon postgress

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,4 +1,4 @@
-import { PrismaBetterSqlite3 } from "@prisma/adapter-better-sqlite3";
+import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "@/generated/prisma/client";
 
 const globalForPrisma = globalThis as unknown as {
@@ -6,14 +6,13 @@ const globalForPrisma = globalThis as unknown as {
 };
 
 function createPrismaClient() {
-  const adapter = new PrismaBetterSqlite3({
-    url: process.env.DATABASE_URL ?? "file:./prisma/dev.db",
+  const adapter = new PrismaPg({
+    connectionString: process.env.DATABASE_URL!,
   });
   return new PrismaClient({ adapter });
 }
 
-export const db =
-  globalForPrisma.prisma ?? createPrismaClient();
+export const db = globalForPrisma.prisma ?? createPrismaClient();
 
 if (process.env.NODE_ENV !== "production") {
   globalForPrisma.prisma = db;
